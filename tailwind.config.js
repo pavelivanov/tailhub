ff4f9abeb2e3e910c388a82156a08c3a6907e25d
Pavel Ivanov <grammka@gmail.com>
2021-01-24 00:11:46 +0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [ './public/index.html' ],
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
